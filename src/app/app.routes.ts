a7bb8055b088d9cc3a64ec2586a530d1c8f6efc5
wesly <wessley.fadi@gmail.com>
2024-10-05 13:14:50 -0400

    fix/properti

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -4,12 +4,13 @@ import { IndexComponent } from './index/index.component';
 import { LoginComponent } from  './auth/pages/login/login.component';  // Asegúrate de que la ruta sea correcta
 import { RegistroComponent } from './auth/pages/registro/registro.component';  // Asegúrate de que la ruta sea correcta
 import { PerfilComponent } from './features/pages/perfil/perfil.component';
+import { PropiedadesComponent } from './features/pages/propiedades/propiedades.component';
 // Definición de rutas
 export const routes: Routes = [
     { path: 'home', component: HomeComponent }, // Página normal
     { path: 'index', component: IndexComponent }, // Página normal
-    { path: 'login', component: LoginComponent }, // Ruta para el login
-    { path: 'perfil', component: PerfilComponent }, // Ruta para el login
+    { path: 'gestion-propiedades', component: PropiedadesComponent }, // Ruta para el login
+    { path: 'mi-perfil', component: PerfilComponent }, // Ruta para el login
     { path: 'login', component: LoginComponent }, // Ruta para el login
     { path: 'registro', component: RegistroComponent }, // Ruta para el registro
 ];
