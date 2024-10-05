1d71bda2e94309c3d01c7cf7301eceabc739836a
wesly <wessley.fadi@gmail.com>
2024-10-05 13:05:03 -0400

    no_images_local

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,12 +3,13 @@ import { HomeComponent } from './home/home.component';
 import { IndexComponent } from './index/index.component';
 import { LoginComponent } from  './auth/pages/login/login.component';  // Asegúrate de que la ruta sea correcta
 import { RegistroComponent } from './auth/pages/registro/registro.component';  // Asegúrate de que la ruta sea correcta
-
+import { PerfilComponent } from './features/pages/perfil/perfil.component';
 // Definición de rutas
 export const routes: Routes = [
     { path: 'home', component: HomeComponent }, // Página normal
     { path: 'index', component: IndexComponent }, // Página normal
     { path: 'login', component: LoginComponent }, // Ruta para el login
+    { path: 'perfil', component: PerfilComponent }, // Ruta para el login
+    { path: 'login', component: LoginComponent }, // Ruta para el login
     { path: 'registro', component: RegistroComponent }, // Ruta para el registro
-    { path: '**', redirectTo: '/home' } // Manejo de rutas desconocidas
 ];
